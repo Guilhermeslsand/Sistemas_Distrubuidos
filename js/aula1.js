50127c9563d539089fa646965e0a94a07f8cffa5
Guilherme Sales de Andrade <825125938@ulife.com.br>
2025-08-18 20:36:40 -0300

    Add folder js

---
+++ js/aula1.js
@@ -0,0 +1,45 @@
+// declarar variaveis e constantes
+//var, const e let
+
+//const -> constantes
+//const nome = 'José';
+//const sobrenome = "Silva";
+
+//interpolação
+//console.log(`${nome} ${sobrenome}`);
+
+//print básico
+//console.log(nome+" "+sobrenome)
+
+//let 
+//let a = 2;
+//console.log(`primeira atribuicao: ${a}`);
+//a = a + 1;
+//console.log(`segunda atribuicao: ${a}`);
+
+//var -> mais antigo
+//var b = 2;
+//console.log(`primeira atribuicao: ${b}`);
+//b = b + 1;
+//console.log(`segunda atribuicao: ${b}`);
+//var linguagem = 'Javasciprit';
+//console.log(`Aprendendo ${linguagem}`);
+//var linguagem = 'Java'
+//console.log(`Aprendendo ${linguagem}`);
+/*
+Porque não usar o var: sempre que for usar novamente a vairavel tem 
+que chamar o var novamente, por isso não é uma boa usar o var
+ */
+//var idade = 19;
+//console.log(`Oi ${nome}`);
+//if(idade >= 18){
+    //var nome = 'João';
+    //console.log(`${nome} pode dirigir`);
+//}
+//console.log(`Até mais, ${nome}`);
+
+//Tipos
+let nome = "Ana"
+console.log(typeof(nome))
+nome = 2
+console.log(typeof(nome))
